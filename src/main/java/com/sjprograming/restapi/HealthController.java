7f0e9b4a1d1f6df09c1add92efa4070706dedf54
Bheemavva <bheemu343@gmail.com>
2026-01-13 22:24:17 +0530

    Fix render deploy issue

--- src/main/java/com/sjprograming/restapi/HealthController.java
+++ src/main/java/com/sjprograming/restapi/HealthController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
-    public String home() {
-        return "OK";
-    }
+//    @GetMapping("/")
+//    public String home() {
+//        return "OK";
+//    }
 
     @GetMapping("/health")
     public String health() {
